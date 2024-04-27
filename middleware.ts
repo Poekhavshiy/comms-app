4af926e761407b859da11ac2c34501bcdb8979ef
Poekhavshiy <hroter.ukr@gmail.com>
2024-04-27 12:11:09 +0300

    apiRoutes

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,15 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
   // Routes that can be accessed while signed out
   publicRoutes: ["/api/uploadthing"],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: ['/no-auth-in-this-route'],
+  // Remove the route from the array to prevent authentication
+  apiRoutes: ['/api/socket/io'] // Remove this line or the specific route if there are multiple
 });
+
  
 export const config = {
   // Protects all routes, including api/trpc.
