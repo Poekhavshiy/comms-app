2e57050af65a45bc253cd5c42bb636a1fbf4b175
Poekhavshiy <hroter.ukr@gmail.com>
2024-06-09 22:14:48 +0300

    Merge remote-tracking branch 'origin/server-switch' into server-switch
    # Conflicts:
    #	.idea/workspace.xml
    #	components/server/server-switch.tsx

--- components/server/server-switch.tsx
+++ components/server/server-switch.tsx
@@ -3,7 +3,7 @@ import {UserButton} from "@clerk/nextjs";
 export function ServerSwitch() {
     return (
         <div
-            className="group px-auto py-5 flex items-center gap-x-2 w-full bg-[neutral-950] hover:bg-neutral-900 dark:hover:bg-neutral-900 transition">
+            className="group px-auto py-4 flex items-center gap-x-2 w-full bg-[neutral-950] hover:bg-neutral-900 dark:hover:bg-neutral-900 transition">
             <div className="pl-2 mt-auto flex-col gap-y-4">
 
                 <UserButton
