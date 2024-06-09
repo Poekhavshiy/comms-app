aae0a977216cf0764b4de0e31db050980c3c1fe8
Poekhavshiy <hroter.ukr@gmail.com>
2024-06-09 23:07:14 +0300

    fine

--- components/server/server-switch.tsx
+++ components/server/server-switch.tsx
@@ -3,9 +3,8 @@ import {UserButton} from "@clerk/nextjs";
 export function ServerSwitch() {
     return (
         <div
-            className="group px-auto py-4 flex items-center gap-x-2 w-full bg-[neutral-950] hover:bg-neutral-900 dark:hover:bg-neutral-900 transition">
+            className="group px-auto py-4 flex items-center gap-x-2 w-full hover:bg-neutral-900 dark:hover:bg-neutral-900 transition">
             <div className="pl-2 mt-auto flex-col gap-y-4">
-
                 <UserButton
                     afterSignOutUrl="/"
                     appearance={{
@@ -14,7 +13,6 @@ export function ServerSwitch() {
                         }
                     }}
                 />
-
             </div>
         </div>
     )
